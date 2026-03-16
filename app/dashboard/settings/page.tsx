876e3fcecda5fb76811607d48868f2614c43fd6c
Felipe Guimarães <guimaraesfelipe6021@gmail.com>
2026-03-16 20:04:52 -0300

    entendendo o fluxo de rotas

---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Settings() {
+  return (
+  <div>
+    <h1>Blog</h1>
+    <Link href="/">Ir para pagina principal</Link>
+  </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
+
   return (
     <div>
+      Home
+      <br />
+      <br />
+     <Link href="/dashboard">ir para Dashboard</Link> <br />
+     <Link href="/blog">Ir para Blog</Link> <br />
+     <Link href="/dashboard/settings">Ir para Settings</Link>
     </div>
   );
 }
